c96b526b15ddcf8fc9d8e4372f2a082e5a309c79
Ali Kemal Çalak <kemalcalak@gmail.com>
2024-05-02 20:39:57 +0300

    Update

--- ileriWebUni/odev1/index2.php
+++ ileriWebUni/odev1/index2.php
@@ -1,28 +1,26 @@
 <?php
-$servername = "localhost";
+$sv = "localhost";
 $username = "root";
 $password = "";
 $dbname = "facebook";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$db = new mysqli($sv, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($db->connect_error) {
+    die("bağlantı hatası: " . $db->connect_error);
 }
 
 @$email = $_POST['email'];
 @$password = $_POST['password'];
 
 $sql = "SELECT * FROM user WHERE passsword = '$password' AND email = '$email' ";
-$result = $conn->query($sql);
+$cikti = $db->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "Login Successful";
+if ($cikti->num_rows > 0) {
+    echo "bağlantı başarılı";
 } else {
     header("Location: http://localhost/Advanced-Web/ileriWebUni/odev1/index.php");
-    echo "Login Failed";
-
+    echo "bağlantı başarısız";
 }
-
-$conn->close();
+$db->close();
 ?>
